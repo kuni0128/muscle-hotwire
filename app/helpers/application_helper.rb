6c8a4b5cc4d36fceab14f21064c70da971133b59
kuniaki <k.ishikawa.0128@gmail.com>
2022-06-17 08:57:34 +0900

    fix: layout by bootstrap

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,13 @@ module ApplicationHelper
   def icon_with_text(icon_name, text)
     tag.span(icon(icon_name), class: "me-2") + tag.span(text)
   end
+
+  def sidebar_link_to(path, emoji, text)
+    classes = %w[my-1 nev-link text-white]
+    classes << "active" if current_page?(path)
+
+    link_to(path, class: classes) do
+      tag.span(class: "me-2") { emoji } + tag.span { text }
+    end
+  end
 end
