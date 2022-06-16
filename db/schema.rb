6f5bf05ac3479352e8193ab963aa7ffc2f8bd6af
kuniaki <k.ishikawa.0128@gmail.com>
2022-06-17 07:19:42 +0900

    fix: add seed

--- db/schema.rb
+++ db/schema.rb
@@ -12,8 +12,8 @@
 
 ActiveRecord::Schema[7.0].define(version: 2022_06_16_001908) do
   create_table "muscles", force: :cascade do |t|
+    t.string "code"
     t.string "name"
-    t.string "name_en"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
